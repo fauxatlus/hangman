9a7ae106d9b50e9d245e02820b671b9b4d9176a0
Ex Novo <randy.a.littlefield@gmail.com>
2018-05-20 01:16:57 -0400

    start of hangman game

---
+++ assets/javascript/game.js
@@ -0,0 +1,100 @@
+//JavaScript Document
+var $ = function (id) {
+    return document.getElementById(id);
+}
+
+
+
+//main variables
+var game = ["pikachu", "bulbasaur", "charmander", "squritle","jigglypuff","meowth","gengar"];
+var choice = Math.floor(Math.random()*7);
+var answer= game[choice];
+var myLength= answer.length;
+var display=[myLength];
+var win = myLength;
+var letters = answer.split('');
+var attemptsLeft= 15;
+var output="";
+var userLetter="";
+
+
+
+//setup game
+var setup = function(){
+for (var i = 0; i < answer.length; i++)
+{
+    display[i] = "_ "; 
+    output = output+display[i]; 
+}
+document.getElementById("game").innerHTML = output;
+output ="";
+}
+
+
+//
+
+function submit()
+{
+
+    output = "";
+    userLetter=$("letter").value;
+    $("letter").value ="";
+
+    for (var c=0; c< answer.length; c++)
+    {
+        
+        if (userLetter === letters[c])
+        {
+            display[c] = userLetter;
+            win --;
+        }
+        output = output + display[c] + " "; 
+        
+        
+    }
+
+
+    document.getElementById("game").innerHTML = output;
+    output="";
+    attemptsLeft--;
+
+
+    
+    if (attemptsLeft < 1) {
+        document.getElementById("guesses").innerHTML = "YOU LOSE";
+    } else if (win < 1) {
+        document.getElementById("guesses").innerHTML = "YOU WIN";
+    } else {
+        document.getElementById("guesses").innerHTML ="You have " + attemptsLeft + " guesses left";
+    }
+}
+
+
+
+
+
+
+
+
+
+
+window.onload= function()
+{
+    setup();
+   
+}
+
+
+
+
+
+
+
+//what needs to be fixed 
+
+//fix correct guesses
+
+//word bank for already guessed letters 
+
+//and if else for no letter input
+
